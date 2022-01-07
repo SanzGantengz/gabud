6852a3e1de7bacf2cf0e98ca7b2b29df8bca7b1a
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2022-01-07 19:39:20 +0700

    Add lagi :v

--- anu.cpp
+++ anu.cpp
@@ -9,7 +9,7 @@ int main()
   
   /**
    * @gabud :v
-   * jadi kwren :v
+   *jadi kwren :v
   */
   
   cout << "pola ke-1\n";
@@ -29,11 +29,11 @@ int main()
   
   cout << "pola ke-2\n\n";
   
-  for(int i = 1; i <= a; i++){
-    for(int b = 1; b <= i; b++){
+  for(int p = 1; p <= a; p++){
+    for(int b = 1; b <= p; b++){
       cout << " ";
     }
-    for(int e = 1; e <= i; e++){
+    for(int e = 1; e <= p; e++){
       cout << " + ";
     }
     cout << endl;
@@ -41,12 +41,21 @@ int main()
   
   cout << "pola ke-3\n\n";
   
-  for(int i = 1; i <= a; i++){
-    for(int b = 1; b <= i; b++){
+  for(int g = 1; g <= a; g++){
+    for(int b = 1; b <= g; b++){
       cout << " ";
     }
-    for(int c = a; c >= i; c--){
-      cout << " * ";
+    for(int c = a; c >= g; c--){
+      cout << " - ";
+    }
+    cout << endl;
+  }
+  
+  cout << "pola ke-4\n\n";
+  
+  for(int f = 1; f <= a; f++){
+    for(int b = 1; b <= f; b++){
+      cout << " $ ";
     }
     cout << endl;
   }
